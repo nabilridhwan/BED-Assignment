5fb0a14eede714b59685add7eecfd7481079df57
Nabil Ridhwan <nabridhwan@gmail.com>
2021-12-28 16:55:38 +0800

    Removed uploading of many files at once

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -5,8 +5,7 @@ cloudinary.config({
     cloud_name: "dqxawxewb",
     api_key: "534278375837782",
     api_secret: "vpVQp-rWDoF5IC68oJCzAS8fDR8",
-    
-}) 
+})
 
 const uploadFileToCloudinary = (buffer, cloudinaryFolderName) => {
     return new Promise(function (resolve, reject) {
@@ -15,29 +14,32 @@ const uploadFileToCloudinary = (buffer, cloudinaryFolderName) => {
             folder: cloudinaryFolderName,
             allowed_formats: "png,jpg",
             resource_type: "image"
-        },(error, result) => {
-            if(result){
+        }, (error, result) => {
+            if (result) {
                 resolve(result)
-            }else{
+            } else {
                 reject(error)
             }
         })
 
         streamifier.createReadStream(buffer).pipe(stream);
-        
+
     }); //End of Promise 
 }
 
 const deleteFileFromCloudinary = (publicId) => {
     return new Promise((resolve, reject) => {
         cloudinary.uploader.destroy(publicId, (error, result) => {
-            if(result){
+            if (result) {
                 resolve(result)
-            }else{
+            } else {
                 reject(error)
             }
         })
     })
 }
 
-module.exports = {uploadFileToCloudinary, deleteFileFromCloudinary} 
+module.exports = {
+    uploadFileToCloudinary,
+    deleteFileFromCloudinary,
+}
